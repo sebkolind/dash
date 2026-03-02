88674df445561df64b9abc3cba8f90301e243b31
sebkolind <sks1993@gmail.com>
2026-03-02 10:19:16 +0100

    fix: add defaults

---
+++ lib/defaults.sh
@@ -0,0 +1,11 @@
+# Whether to show the header. Default: true
+D_HEADER=true
+
+# Whether to show the header logo. Default: true
+D_HEADER_LOGO=true
+
+# Whether to show the header metadata. Default: true
+D_HEADER_META=true
+
+# Whether to show the footer. Default: true
+D_FOOTER=true
